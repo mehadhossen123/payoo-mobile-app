7839390d11c9b1234641ebb6501c369b165bc94a
Mehad Hossen <mehad004938@gmail.com>
2025-08-23 13:21:48 +0600

    add my front page

---
+++ index.js
@@ -0,0 +1,20 @@
+document.getElementById("loigin-button").addEventListener('click',function(e){
+    e.preventDefault();
+    const number=12345678910;
+    const pin=1977;
+    const mobileNumber = document.getElementById("mobile-number").value
+    const mobileNumberConverted=parseInt(mobileNumber);
+    const pinNumber = document.getElementById("pin-number").value;
+    const pinNumberConverted=parseInt(pinNumber);
+    if(mobileNumberConverted===number&& pinNumberConverted===pin){
+        window.location.href="./homePage.html";
+        
+    }
+    else{
+     alert("Invalid match");
+        
+    }
+    
+
+    
+})
