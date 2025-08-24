aca976bd2d0e59c3b53b0dddc22c705882715238
Mehad Hossen <mehad004938@gmail.com>
2025-08-24 11:06:19 +0600

    please add  my toggling fiel

--- home.js
+++ home.js
@@ -1,13 +1,22 @@
 const validPin=1234;
+// reuseable function start hrom here 
+function getInputValueNumber(id){
+   const getValueNumber= parseInt(document.getElementById(id).value);
+   return getValueNumber;
+   
+}
+function getValue(id){
+   const getValue=document.getElementById(id).value;
+   return getValue;
+}
 
-// add monet js start here 
+// add money js start here 
 document.getElementById("add-monet-btn").addEventListener("click",function(e){
     e.preventDefault();
- const bank = document.getElementById("select-bank").value;
- const bankAccount = document.getElementById("bank-account").value;
- const numberPin = parseInt(document.getElementById("number-pin").value);
- const amountAdd = parseInt(document.getElementById("amount-add").value);
- 
+ const bank = getValue("select-bank");
+ const bankAccount = getValue("bank-account");
+ const numberPin = getInputValueNumber("number-pin");
+ const amountAdd = getInputValueNumber("amount-add");
  const beforeBalance = parseInt(
    document.getElementById("before-balance").innerText
  );
@@ -28,29 +37,60 @@ document.getElementById("add-monet-btn").addEventListener("click",function(e){
 });
 // toggling feature start here 
 document.getElementById("add-money-taka").addEventListener('click',function(){
+   const form= document.getElementsByClassName("form");
+   for(const forms of form){
+      forms.style.display="none";
+   }
     document.getElementById("add-money-parent").style.display = "block";
-    document.getElementById("cash-out-parent").style.display="none";
+    
+
 })
 
 document.getElementById("cash-out-taka").addEventListener("click", function () {
+    const form = document.getElementsByClassName("form");
+    for (const forms of form) {
+      forms.style.display = "none";
+    }
   document.getElementById("cash-out-parent").style.display = "block";
-  document.getElementById("add-money-parent").style.display='none';
+  
+
 
 });
+
+document.getElementById("transfer-money").addEventListener('click' ,function(){
+    const form = document.getElementsByClassName("form");
+    for (const forms of form) {
+      forms.style.display = "none";
+    }
+   
+    document.getElementById("transfer-monet-parent").style.display='block';
+
+
+})
+
+document.getElementById("get-bonus").addEventListener('click',function(){
+    const form = document.getElementsByClassName("form");
+    for (const forms of form) {
+      forms.style.display = "none";
+    }
+    document.getElementById("get-bonus -parent").style.display='block';
+
+})
+
+
 // cash out js start here 
 
 document.getElementById("widthdraw-money-btn").addEventListener('click',function(e){
     e.preventDefault();
-    const widthdrawAmount = parseInt(
-      document.getElementById("with-draw-amount").value
-    );
+    const widthdrawAmount = getInputValueNumber('with-draw-amount');
+    
     
  const availBalance = parseInt(
    document.getElementById("before-balance").innerText);
-   const agentId = document.getElementById("agent").value;
+   const agentId = getValue("agent");
    const pipi=4545;
   
-  const pin = parseInt(document.getElementById("pin").value);
+  const pin = getInputValueNumber("pin");
  if(agentId.length !==11){
     alert("Please provide valid agent number");
     return;
