13f543648d6a0a968993e6fd321a9f77ce83744a
Mehad Hossen <mehad004938@gmail.com>
2025-08-24 19:29:33 +0600

    add my last file

--- home.js
+++ home.js
@@ -1,4 +1,9 @@
 const validPin=1234;
+const transactionData=[];
+
+
+
+
 // reuseable function start hrom here 
 function getInputValueNumber(id){
    const getValueNumber= parseInt(document.getElementById(id).value);
@@ -9,6 +14,32 @@ function getValue(id){
    const getValue=document.getElementById(id).value;
    return getValue;
 }
+// Reuseable toggling function
+function getToggle(id){
+   const form = document.getElementsByClassName("form");
+   for (const forms of form) {
+     forms.style.display = "none";
+   }
+   document.getElementById(id).style.display = "block";
+   return;
+    
+}
+// handle toggling button here 
+function toggleButtonHandle(id){
+    const formBtns = document.getElementsByClassName(" form-btn");
+    for (const btn of formBtns) {
+      btn.classList.remove("border-[#0874f2]", "bg-[#0874f20d]");
+      btn.classList.add("border-gray-300");
+    }
+    document
+      .getElementById(id)
+      .classList.remove("border-[#08080819]");
+
+    document
+      .getElementById(id)
+      .classList.add("border-[#0874f2]", "bg-[#0874f20d]");
+
+}
 
 // add money js start here 
 document.getElementById("add-monet-btn").addEventListener("click",function(e){
@@ -17,6 +48,10 @@ document.getElementById("add-monet-btn").addEventListener("click",function(e){
  const bankAccount = getValue("bank-account");
  const numberPin = getInputValueNumber("number-pin");
  const amountAdd = getInputValueNumber("amount-add");
+ if(amountAdd<=0){
+   alert("Invalid Amount ");
+   return;
+ }
  const beforeBalance = parseInt(
    document.getElementById("before-balance").innerText
  );
@@ -31,51 +66,103 @@ document.getElementById("add-monet-btn").addEventListener("click",function(e){
  
  const totalBalance=beforeBalance+amountAdd;
  document.getElementById("before-balance").innerText=totalBalance;
- console.log(totalBalance);
+// console.log(totalBalance);
+const data={
+   name:"Add Money",
+   date: new Date().toLocaleTimeString()
+
+
+}
+ transactionData.push(data);
+ 
  
     
 });
+document.getElementById("transaction-money").addEventListener('click',function(){
+  
+   const transcationContainer= document.getElementById("transaction-container");
+    transcationContainer.innerText = "";
+   
+   for(const data of transactionData){
+      const div =document.createElement('div');
+      div.innerHTML = `
+          <div class="bg-[#0808080d] mt-3  rounded-xl flex justify-between items-center">
+    <div class="flex items-center gap-4">
+    <div class="rounded-full  p-4 bg-[#0808080d]">
+         <img  src="image/wallet1.png" alt="" class="mx-auto">
+    </div>
+    <div>
+        <h1>${data.name}</h1>
+        <p>${data.date}</p>
+    </div>
+</div>
+<i class="fa-solid fa-ellipsis-vertical"></i>
+
+</div>
+      `;
+      transcationContainer.appendChild(div);
+      
+   }
+
+
+
+
+
+})
 // toggling feature start here 
 document.getElementById("add-money-taka").addEventListener('click',function(){
-   const form= document.getElementsByClassName("form");
-   for(const forms of form){
-      forms.style.display="none";
-   }
-    document.getElementById("add-money-parent").style.display = "block";
+   getToggle("add-money-parent");
+  toggleButtonHandle("add-money-taka");
     
 
 })
 
+
+
+
 document.getElementById("cash-out-taka").addEventListener("click", function () {
-    const form = document.getElementsByClassName("form");
-    for (const forms of form) {
-      forms.style.display = "none";
-    }
-  document.getElementById("cash-out-parent").style.display = "block";
-  
+    getToggle("cash-out-parent");
+    toggleButtonHandle("cash-out-taka");
 
 
 });
 
+
+
+
+
 document.getElementById("transfer-money").addEventListener('click' ,function(){
-    const form = document.getElementsByClassName("form");
-    for (const forms of form) {
-      forms.style.display = "none";
-    }
-   
-    document.getElementById("transfer-monet-parent").style.display='block';
+    
+   getToggle("transfer-monet-parent");
+   toggleButtonHandle("transfer-money");
+
 
 
 })
+
+
+
 
 document.getElementById("get-bonus").addEventListener('click',function(){
-    const form = document.getElementsByClassName("form");
-    for (const forms of form) {
-      forms.style.display = "none";
-    }
-    document.getElementById("get-bonus -parent").style.display='block';
-
+   getToggle("get-bonus -parent");
+     toggleButtonHandle("get-bonus");
+  
 })
+
+
+document.getElementById("pay-bill").addEventListener("click", function () {
+  getToggle("pay-bill -parent");
+  toggleButtonHandle("pay-bill");
+});
+
+
+
+
+document
+  .getElementById("transaction-money").addEventListener("click", function () {
+    getToggle("transaction -parent");
+    toggleButtonHandle("transaction-money");
+  });
 
 
 // cash out js start here 
@@ -87,6 +174,10 @@ document.getElementById("widthdraw-money-btn").addEventListener('click',function
     
  const availBalance = parseInt(
    document.getElementById("before-balance").innerText);
+   if(availBalance<widthdrawAmount || widthdrawAmount<=0){
+      alert("Invalid amount ");
+      return ;
+   }
    const agentId = getValue("agent");
    const pipi=4545;
   
@@ -103,6 +194,14 @@ document.getElementById("widthdraw-money-btn").addEventListener('click',function
 
 const updateBalance=availBalance-widthdrawAmount;
 document.getElementById("before-balance").innerText=updateBalance;
+const data={
+   name:"Cash Out ",
+   date: new Date().toLocaleTimeString()
 
 
+}
+ transactionData.push(data);
+ console.log(transactionData);
+    
 });
+
